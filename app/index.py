e6be5b2f9773097b8141c139ab2c9eada105349f
Roberto Carlos Ricci <roberto.rricci@gmail.com>
2025-04-02 18:54:18 -0300

    Update index.py

--- app/index.py
+++ app/index.py
@@ -1,12 +1,19 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+
+# Carregar as variáveis do arquivo .env
+load_dotenv()
 
 def main():
+    database_url = os.getenv('DATABASE_URL')
     st.title('Aplicativo Simples com Streamlit')
 
     user_input = st.text_input("Digite algo:")
     
     if user_input:
         st.write(f'Você digitou: {user_input}')
+        st.write(f'valor: {database_url}')
 
 if __name__ == "__main__":
     main()
