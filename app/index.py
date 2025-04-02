29157b9225d0d2a80a6cfe15e050aa632900db3b
Roberto Carlos Ricci <roberto.rricci@gmail.com>
2025-04-02 19:56:42 -0300

    Update index.py

--- app/index.py
+++ app/index.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-    st.write(os.getenv())
+    st.write(os.getenv('DATABASE_URL'))
     print(os.getenv('DATABASE_URL'))
     database_url = os.getenv('DATABASE_URL')
     st.title('Aplicativo Simples com Streamlit')
